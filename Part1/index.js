dc42e531cef7fad56dc19d6bd64623c01460a7a2
Maikalanimai <kalonekamimoto@gmail.com>
2019-11-26 15:14:38 -0700

    fully complete

--- Part1/index.js
+++ Part1/index.js
@@ -1,15 +1,17 @@
-let board = [,,,,,,,,]
-function play(box){
+let board = [, , , , , , , ,]
+function play(box) {
     let playerSpan = document.getElementById('player')
     let clicked = document.getElementById(box)
-    if(playerSpan.innerText === 'X'){
-        playerSpan.innerText = 'O'
-        clicked.innerText = 'X'
-        board[box] ='X'
-    } else{
-        playerSpan.innerText = 'X'
-        clicked.innerText = 'O'
-        board[box] = 'O'
+    if (board[box] !== 'X' && board[box] !== 'O') {
+        if (playerSpan.innerText === 'X') {
+            playerSpan.innerText = 'O'
+            clicked.innerText = 'X'
+            board[box] = 'X'
+        } else {
+            playerSpan.innerText = 'X'
+            clicked.innerText = 'O'
+            board[box] = 'O'
+        }
     }
     const topRight = board[0]
     const topMiddle = board[1]
@@ -21,34 +23,34 @@ function play(box){
     const bottomMiddle = board[7]
     const bottomLeft = board[8]
     // console.log(topRight, topMiddle, topLeft)
-    if(topRight !== undefined && topRight === topMiddle && topMiddle === topLeft){
+    if (topRight !== undefined && topRight === topMiddle && topMiddle === topLeft) {
         alert(`Player ${topRight} Wins!`)
         window.location.reload(false)
-    }else if(topRight !== undefined && centerRight === topRight && topRight === bottomRight){
+    } else if (topRight !== undefined && centerRight === topRight && topRight === bottomRight) {
         alert(`Player ${centerRight} Wins!`)
         window.location.reload(false)
-    }else if(topRight !== undefined && topRight === centerMiddle && centerMiddle === bottomLeft){
+    } else if (topRight !== undefined && topRight === centerMiddle && centerMiddle === bottomLeft) {
         alert(`Player ${topRight} Wins!`)
         window.location.reload(false)
-    }else if(topMiddle !== undefined && topMiddle === centerMiddle && centerMiddle === bottomMiddle){
+    } else if (topMiddle !== undefined && topMiddle === centerMiddle && centerMiddle === bottomMiddle) {
         alert(`Player ${topMiddle} Wins!`)
         window.location.reload(false)
-    }else if(topLeft !== undefined && topLeft === centerMiddle && centerMiddle === bottomRight){
+    } else if (topLeft !== undefined && topLeft === centerMiddle && centerMiddle === bottomRight) {
         alert(`Player ${topLeft} Wins!`)
         window.location.reload(false)
-    }else if(centerLeft !== undefined && centerLeft === centerMiddle && centerMiddle === centerRight){
+    } else if (centerLeft !== undefined && centerLeft === centerMiddle && centerMiddle === centerRight) {
         alert(`Player ${centerLeft} Wins!`)
         window.location.reload(false)
-    }else if(topLeft !== undefined && topLeft === centerLeft && centerLeft === bottomLeft){
+    } else if (topLeft !== undefined && topLeft === centerLeft && centerLeft === bottomLeft) {
         alert(`Player ${topLeft} Wins!`)
         window.location.reload(false)
-    }else if(bottomRight !== undefined && bottomRight === bottomMiddle && bottomMiddle === bottomLeft){
+    } else if (bottomRight !== undefined && bottomRight === bottomMiddle && bottomMiddle === bottomLeft) {
         alert(`Player ${bottomRight} Wins!`)
         window.location.reload(false)
-    } else if(topRight !== undefined && topMiddle !== undefined && topLeft !== undefined && centerRight !== undefined && centerMiddle !== undefined && centerLeft !== undefined && bottomRight !== undefined && bottomMiddle !== undefined && bottomLeft !== undefined){
+    } else if (topRight !== undefined && topMiddle !== undefined && topLeft !== undefined && centerRight !== undefined && centerMiddle !== undefined && centerLeft !== undefined && bottomRight !== undefined && bottomMiddle !== undefined && bottomLeft !== undefined) {
         alert(`No Winner! Refresh the page and try again!`)
         window.location.reload(false)
-    } 
+    }
     console.log([board])
-    
+
 }
