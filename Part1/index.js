b40cf95728bb1874e71130cffa1ff6af737b5d3f
Maikalanimai <kalonekamimoto@gmail.com>
2019-11-26 15:02:32 -0700

    complete, semi progress on bonus

---
+++ Part1/index.js
@@ -0,0 +1,54 @@
+let board = [,,,,,,,,]
+function play(box){
+    let playerSpan = document.getElementById('player')
+    let clicked = document.getElementById(box)
+    if(playerSpan.innerText === 'X'){
+        playerSpan.innerText = 'O'
+        clicked.innerText = 'X'
+        board[box] ='X'
+    } else{
+        playerSpan.innerText = 'X'
+        clicked.innerText = 'O'
+        board[box] = 'O'
+    }
+    let topRight = board[0]
+    let topMiddle = board[1]
+    let topLeft = board[2]
+    let centerRight = board[3]
+    let centerMiddle = board[4]
+    let centerLeft = board[5]
+    let bottomRight = board[6]
+    let bottomMiddle = board[7]
+    let bottomLeft = board[8]
+    console.log(topRight, topMiddle, topLeft)
+    if(topRight !== undefined && topRight === topMiddle && topMiddle === topLeft){
+        alert(`Player ${topRight} Wins!`)
+        window.location.reload(false)
+    }else if(topRight !== undefined && centerRight === topRight && topRight === bottomRight){
+        alert(`Player ${centerRight} Wins!`)
+        window.location.reload(false)
+    }else if(topRight !== undefined && topRight === centerMiddle && centerMiddle === bottomLeft){
+        alert(`Player ${topRight} Wins!`)
+        window.location.reload(false)
+    }else if(topMiddle !== undefined && topMiddle === centerMiddle && centerMiddle === bottomMiddle){
+        alert(`Player ${topMiddle} Wins!`)
+        window.location.reload(false)
+    }else if(topLeft !== undefined && topLeft === centerMiddle && centerMiddle === bottomRight){
+        alert(`Player ${topLeft} Wins!`)
+        window.location.reload(false)
+    }else if(centerLeft !== undefined && centerLeft === centerMiddle && centerMiddle === centerRight){
+        alert(`Player ${centerLeft} Wins!`)
+        window.location.reload(false)
+    }else if(topLeft !== undefined && topLeft === centerLeft && centerLeft === bottomLeft){
+        alert(`Player ${topLeft} Wins!`)
+        window.location.reload(false)
+    }else if(bottomRight !== undefined && bottomRight === bottomMiddle && bottomMiddle === bottomLeft){
+        alert(`Player ${bottomRight} Wins!`)
+        window.location.reload(false)
+    } else if(topRight !== undefined && topMiddle !== undefined && topLeft !== undefined && centerRight !== undefined && centerMiddle !== undefined && centerLeft !== undefined && bottomRight !== undefined && bottomMiddle !== undefined && bottomLeft !== undefined){
+        alert(`No Winner! Refresh the page and try again!`)
+        window.location.reload(false)
+    } 
+    console.log([board])
+    
+}
